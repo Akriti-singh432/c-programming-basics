35b5f93d35191bf227f3f4bea56bce00aa4eb233
Akriti Singh <akritisingh101010@gmail.com>
2026-04-06 12:53:45 +0530

    Added new C program

---
+++ Functionsum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int calsum(int x, int y, int z);
+int main(){
+    int a,b,c,sum;
+    printf("Enter a first numbers");
+    scanf("%d",&a);
+     printf("Enter a Second numbers");
+      scanf("%d", &b);
+      printf("Enter a Third numbers");
+       scanf("%d", &c);
+    sum=calsum(a,b,c);
+    printf("sum=%d\n",sum);
+    return 0;
+}
+int calsum(int x, int y, int z)
+{
+    int d;
+    d=x+y+z;
+    return(d);
+}
